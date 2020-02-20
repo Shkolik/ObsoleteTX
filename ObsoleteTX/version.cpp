53afc94d8a01fa5e5aa27596aff31e292427abbc
Andrew Shkolik <shkolik@gmail.com>
2020-02-19 22:39:03 -0600

    ...

--- ObsoleteTX/version.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * version.cpp
- *
- * Created: 1/29/2020 10:23:06 PM
- *  Author: andrew.shkolik
- */ 
-#include "verion.h"
-
-const char vers_stamp[] PROGMEM = "FW: " "ObsoleteTX" "\036VERS\037\033: 1" "\036DATE\037\033: " "\036EEPR\037\033: " EEPROM_STR;
